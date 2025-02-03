265d16bbc3df2ea191a0a66debb10dbd12b0eed9
saeto <dom.serwe@gmail.com>
2025-02-03 16:02:24 -0500

    moved all the physics stuff into its own file

--- src/game.ts
+++ src/game.ts
@@ -1,32 +1,7 @@
 import { Vec2, Rect } from "./math.js"
 
 import * as graphics from "./graphics.js"
-
-type AABB = {
-    position: Vec2
-    halfSize: Vec2
-}
-
-type PhysicsBody = {
-    position: Vec2
-    velocity: Vec2
-    size: Vec2
-    mass: number
-
-    name: string
-    color: string
-}
-
-type ContactPoint = {
-    normal: Vec2
-    penetration: number
-}
-
-type CollisionInfo = {
-    first: PhysicsBody
-    second: PhysicsBody
-    point: ContactPoint
-}
+import * as physics from "./physics.js"
 
 function randomRange(min: number, max: number): number {
     return Math.random() * (max - min) + min
@@ -39,222 +14,132 @@ function randomInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function isAABBColliding(a: AABB, b: AABB): boolean {
-    const delta = b.position.copy().sub(a.position)
-    const totalSize = a.halfSize.copy().add(b.halfSize)
-
-    return Math.abs(delta.x) < totalSize.x && Math.abs(delta.y) < totalSize.y
+const world: physics.World = {
+    gravity: new Vec2(0, 0),
+    entities: []
 }
-
-function AABBCollisionInfo(a: AABB, b: AABB, info: CollisionInfo): boolean {
-    if (!isAABBColliding(a, b)) {
-        return false
-    }
-
-    const faces = [
-        new Vec2(-1, 0), new Vec2(1, 0),
-        new Vec2(0, -1), new Vec2(0, 1),
-    ]
-
-    const maxA = a.position.copy().add(a.halfSize)
-    const minA = a.position.copy().sub(a.halfSize)
-
-    const maxB = b.position.copy().add(b.halfSize)
-    const minB = b.position.copy().sub(b.halfSize)
-
-    const distances = [
-        maxB.x - minA.x,
-        maxA.x - minB.x,
-        maxB.y - minA.y,
-        maxA.y - minB.y,
-    ]
-
-    let penetration = Number.MAX_VALUE
-    let bestAxis = new Vec2(0, 0)
-
-    for (let i = 0; i < faces.length; i++) {
-        if (distances[i] < penetration) {
-            penetration = distances[i]
-            bestAxis = faces[i]
-        }
-    }
-
-    info.point.normal = bestAxis
-    info.point.penetration = penetration
-
-    return true
-}
-
-const world: PhysicsBody[] = []
-
-let aDown = false
-let dDown = false
-
-let sDown = false
 
 export function init() {
-    world.push({
-        position: new Vec2(window.innerWidth / 2, window.innerHeight / 2),
-        velocity: new Vec2(0, 0),
-        size: new Vec2(32, 32),
-        mass: 60,
+    for (let i = 0; i < 100; i++) {
+        const colors = [ "red", "orange", "yellow", "cyan", "blue" ]
+
+        world.entities.push({
+            id: 0,
+
+            name: "",
+            color: colors[randomInt(0, colors.length - 1)],
+
+            position: new Vec2(randomRange(48, window.innerWidth - 48), randomRange(48, window.innerHeight - 48)),
+            velocity: new Vec2(randomRange(-300, 300), randomRange(-300, 300)),
+            //velocity: new Vec2(0, 0),
+
+            size: new Vec2(32, 32),
+            mass: randomRange(1, 100),
+            restitution: 0.67,
+        })
+    }
+
+    /*
+    // place player
+    world.entities.push({
+        id: 0,
 
         name: "player",
         color: "orange",
+
+        position: new Vec2(window.innerWidth / 2, window.innerHeight / 2),
+        velocity: new Vec2(0, 0),
+        
+        size: new Vec2(32, 32),
+        mass: 60,
+        restitution: 0.67,
     })
+    */
 
     // place walls
-    world.push({
+    world.entities.push({
+        id: 0,
+
+        name: "wall",
+        color: "black",
+
         position: new Vec2(window.innerWidth / 2, 0),
         velocity: new Vec2(0, 0),
+        
         size: new Vec2(window.innerWidth, 0),
         mass: 0,
-        name: "wall",
-        color: "black",
+        restitution: 1,
     })
 
-    world.push({
+    world.entities.push({
+        id: 0,
+
+        name: "wall",
+        color: "black",
+
         position: new Vec2(window.innerWidth / 2, window.innerHeight),
         velocity: new Vec2(0, 0),
+        
         size: new Vec2(window.innerWidth, 0),
         mass: 0,
+        restitution: 1,
+    })
+    
+    world.entities.push({
+        id: 0,
+
         name: "wall",
         color: "black",
-    })
 
-    world.push({
         position: new Vec2(0, window.innerHeight / 2),
         velocity: new Vec2(0, 0),
+        
         size: new Vec2(0, window.innerHeight),
         mass: 0,
+        restitution: 1,
+    })
+    
+    world.entities.push({
+        id: 0,
+
         name: "wall",
         color: "black",
-    })
 
-    world.push({
         position: new Vec2(window.innerWidth, window.innerHeight / 2),
         velocity: new Vec2(0, 0),
+        
         size: new Vec2(0, window.innerHeight),
         mass: 0,
-        name: "wall",
-        color: "black",
+        restitution: 1,
     })
 }
 
 export function update(deltaTime: number) {
-    let player: PhysicsBody | null = null
-
-    for (let i = 0; i < world.length; i++) {
-        if (world[i].name === "player") {
-            player = world[i]
-        }
-    }
-
-    if (player) {
-        if (sDown) {
-            player.velocity.y = -100
-        }
-
-        player.velocity.x = ((dDown ? 1 : 0) - (aDown ? 1 : 0)) * 150
-    }
-
-    // update positions
-    for (const body of world) {
-        if (body.mass === 0) {
-            continue
-        }
-
-        body.velocity.add(new Vec2(0, 980).scale(deltaTime))
-        body.position.add(body.velocity.copy().scale(deltaTime))
-    }
-
-    // check collision
-    for (let i = 0; i < world.length - 1; i++) {
-        for (let j = i + 1; j < world.length; j++) {
-            const first = world[i]
-            const second = world[j]
-
-            const info = {
-                first: first,
-                second: second,
-                point: { normal: new Vec2(0, 0), penetration: 0 }
-            }
-
-            const result = AABBCollisionInfo(
-                { position: first.position.copy(), halfSize: first.size.copy().scale(0.5) },
-                { position: second.position.copy(), halfSize: second.size.copy().scale(0.5) },
-                info)
-
-            if (result) {
-                const firstInvMass = first.mass != 0 ? (1.0 / first.mass) : 0
-                const secondInvMass = second.mass != 0 ? (1.0 / second.mass) : 0
-
-                const totalMass = firstInvMass + secondInvMass
-
-                const firstPMass = totalMass != 0 ? (firstInvMass / totalMass) : 0
-                const secondPMass = totalMass != 0 ? (secondInvMass / totalMass) : 0
-
-                first.position.sub(info.point.normal.copy().scale(info.point.penetration * firstPMass))
-                second.position.add(info.point.normal.copy().scale(info.point.penetration * secondPMass))
-
-                const restitution = 0.67
-
-                if (info.point.normal.x == 1 || info.point.normal.x == -1) {
-                    first.velocity.x *= -restitution
-                    second.velocity.x *= -restitution
-                }
-
-                if (info.point.normal.y == 1 || info.point.normal.y == -1) {
-                    first.velocity.y *= -restitution
-                    second.velocity.y *= -restitution
-                }
-            }
-        }
-    }
+    physics.step(world, deltaTime)
+    physics.maybeResolveCollisions(world)
 }
+
 
 export function render(ctx: graphics.RenderContext) {
     graphics.clearScreen(ctx, "rgb(100, 100, 100)")
 
-    for (const body of world) {
-        graphics.drawRectLines(ctx, body.position, body.size, body.color)
+    for (const entity of world.entities) {
+        graphics.drawRect(ctx, entity.position, entity.size, entity.color)
     }
+
+    let totalK = 0
+    let totalP = 0
+
+    for (const entity of world.entities) {
+        totalK += 0.5 * entity.mass * entity.velocity.dot(entity.velocity)
+        totalP += world.gravity.y * entity.mass * (window.innerHeight - entity.position.y)
+    }
+
+    const totalE = totalK + totalP
+
+    ctx.ctx.font = "16px Arial"
+    ctx.ctx.fillStyle = "black"
+    ctx.ctx.fillText("Total Kinetic Energy: " + totalK, 32, 32)
+    ctx.ctx.fillText("Total Potential Energy " + totalP, 32, 64)
+    ctx.ctx.fillText("Total Energy: " + totalE, 32, 96)
 }
-
-document.addEventListener("keydown", (e) => {
-    switch (e.key) {
-        case 'a': {
-            aDown = true
-            break
-        }
-
-        case 'd': {
-            dDown = true
-            break
-        }
-
-        case 's': {
-            sDown = true
-            break
-        }
-    }
-})
-
-document.addEventListener("keyup", (e) => {
-    switch (e.key) {
-        case 'a': {
-            aDown = false
-            break
-        }
-
-        case 'd': {
-            dDown = false
-            break
-        }
-
-        case 's': {
-            sDown = false
-            break
-        }
-    }
-})
---
+++ src/physics.ts
@@ -0,0 +1,128 @@
+import { Vec2 } from "./math.js"
+
+export type Entity = {
+    id: number
+
+    name: string
+    color: string
+
+    position: Vec2
+    velocity: Vec2
+
+    size: Vec2
+    mass: number
+    restitution: number
+}
+
+export type World = {
+    gravity: Vec2
+    entities: Entity[]
+}
+
+type AABB = {
+    position: Vec2
+    halfSize: Vec2
+}
+
+type ContactPoint = {
+    normal: Vec2
+    penetration: number
+}
+
+function isAABBColliding(a: AABB, b: AABB): boolean {
+    const delta = b.position.copy().sub(a.position)
+    const totalSize = a.halfSize.copy().add(b.halfSize)
+
+    return Math.abs(delta.x) < totalSize.x && Math.abs(delta.y) < totalSize.y
+}
+
+ function AABBCollisionInfo(a: AABB, b: AABB): { colliding: boolean, point: ContactPoint | null } {
+    if (!isAABBColliding(a, b)) {
+        return { colliding: false, point: null }
+    }
+
+    const faces = [
+        new Vec2(-1, 0), new Vec2(1, 0),
+        new Vec2(0, -1), new Vec2(0, 1),
+    ]
+
+    const maxA = a.position.copy().add(a.halfSize)
+    const minA = a.position.copy().sub(a.halfSize)
+
+    const maxB = b.position.copy().add(b.halfSize)
+    const minB = b.position.copy().sub(b.halfSize)
+
+    const distances = [
+        maxB.x - minA.x,
+        maxA.x - minB.x,
+        maxB.y - minA.y,
+        maxA.y - minB.y,
+    ]
+
+    let penetration = Number.MAX_VALUE
+    let bestAxis = new Vec2(0, 0)
+
+    for (let i = 0; i < faces.length; i++) {
+        if (distances[i] < penetration) {
+            penetration = distances[i]
+            bestAxis = faces[i]
+        }
+    }
+
+    return { colliding: true, point: { normal: bestAxis, penetration: penetration }}
+}
+
+function resolveAABBCollision(a: Entity, b: Entity, point: ContactPoint) {
+    const aInvMass = a.mass !== 0 ? 1 / a.mass : 0
+    const bInvMass = b.mass !== 0 ? 1 / b.mass : 0
+
+    const totalMass = aInvMass + bInvMass
+
+    const aPMass = totalMass !== 0 ? aInvMass / totalMass : 0
+    const bPMass = totalMass !== 0 ? bInvMass / totalMass : 0
+
+    a.position.sub(point.normal.copy().scale(point.penetration * aPMass))
+    b.position.add(point.normal.copy().scale(point.penetration * bPMass))
+
+    const effectiveRestitution = Math.sqrt(a.restitution * b.restitution)
+
+    if (point.normal.x === 1 || point.normal.x === -1) {
+        a.velocity.x *= -effectiveRestitution
+        b.velocity.x *= -effectiveRestitution
+    }
+    
+    if (point.normal.y === 1 || point.normal.y === -1) {
+        a.velocity.y *= -effectiveRestitution
+        b.velocity.y *= -effectiveRestitution
+    }
+}
+
+export function step(world: World, deltaTime: number) {
+    for (const entity of world.entities) {
+        if (entity.mass === 0) {
+            continue
+        }
+
+        entity.velocity.add(world.gravity.copy().scale(deltaTime))
+        entity.position.add(entity.velocity.copy().scale(deltaTime))
+    }
+}
+
+export function maybeResolveCollisions(world: World) {
+    for (let i = 0; i < world.entities.length - 1; i++) {
+        for (let j = i + 1; j < world.entities.length; j++) {
+            const a = world.entities[i]
+            const b = world.entities[j]
+
+            const result = AABBCollisionInfo(
+                { position: a.position.copy(), halfSize: a.size.copy().scale(0.5) },
+                { position: b.position.copy(), halfSize: b.size.copy().scale(0.5) })
+
+            if (!result.colliding || result.point == null) {
+                continue
+            }
+
+            resolveAABBCollision(a, b, result.point)
+        }
+    }
+}
